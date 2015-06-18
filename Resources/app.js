2a403fb7aeeff2ec2c85614a9024f145d6b9d9ab
Architeuthis_Rex <jonathanvenn@gmail.com>
2015-06-18 13:04:50 +0800

    Tableview started, custom views working

--- Resources/app.js
+++ Resources/app.js
@@ -1,64 +1,88 @@
 // this sets the background color of the master UIView (when there are no windows/tab groups on it)
 Titanium.UI.setBackgroundColor('#000');
 
-// create tab group
-var tabGroup = Titanium.UI.createTabGroup();
 
 
-//
-// create base UI tab and root window
-//
+
+var sites = null,
+	indexes = [];
+//Set Default Font	
+var defaultFontSize = Ti.Platform.name === 'android' ? 16 : 14;
+
+
+
+
+/**
+	 * Access the FileSystem Object to read in the information from a flat file (lib/userData/data.js)
+	 * DOCS: http://docs.appcelerator.com/platform/latest/#!/api/Titanium.Filesystem
+	 */
+	var file = Ti.Filesystem.getFile(Ti.Filesystem.resourcesDirectory + "sites/sites.json"); 
+	
+	/**
+	 * Populate the users variable from the file this call returns an array
+	 */
+//populate sites array with data from sites.json
+sites = JSON.parse(file.read().text).sites;
+//function to sort the array
+var sort_by = function(field, reverse, primer){
+
+   var key = primer ? 
+       function(x) {return primer(x[field])} : 
+       function(x) {return x[field]};
+   reverse = !reverse ? 1 : -1;
+
+   return function (a, b) {
+       return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+     } 
+}
+//sort the array by name	
+sites.sort(sort_by('name', false, function(a){return a.toUpperCase()}));
+//create the window
 var win1 = Titanium.UI.createWindow({  
-    title:'Tab 1',
-    backgroundColor:'#fff'
+    title:'Puerto Galera Dive Sites',
+    backgroundColor:'black'
 });
+
+//create a tab
 var tab1 = Titanium.UI.createTab({  
     icon:'KS_nav_views.png',
-    title:'Tab 1',
+    title:'Dive Sites',
     window:win1
 });
 
-var label1 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 1',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
+//create the table data
+var tbl_data = [];
+for (var i=0; i < sites.length; i++) {
+  var row = Ti.UI.createTableViewRow({
+  	classname: 'sitesList',
+  	rowIndex: i,
+  	height: 80
+  });
+  var diveSiteNameLabel = Ti.UI.createLabel({
+    font:{fontFamily:'Arial', fontSize:defaultFontSize+6, fontWeight:'bold'},
+    text:sites[i].name,
+    left:6,
+    top: 6,
+    width:200, height: 30
+  });
+  
+  var maxDepthLabel = Ti.UI.createLabel({
+    font:{fontFamily:'Arial', fontSize:defaultFontSize},
+    text:sites[i].maxDepth,
+    left:6,
+    top: 40,
+    width:200, height: 30
+  });
+  row.add(diveSiteNameLabel);
+  row.add(maxDepthLabel);
+  tbl_data.push(row);
+};
 
-win1.add(label1);
-
-//
-// create controls tab and root window
-//
-var win2 = Titanium.UI.createWindow({  
-    title:'Tab 2',
-    backgroundColor:'#fff'
-});
-var tab2 = Titanium.UI.createTab({  
-    icon:'KS_nav_ui.png',
-    title:'Tab 2',
-    window:win2
-});
-
-var label2 = Titanium.UI.createLabel({
-	color:'#999',
-	text:'I am Window 2',
-	font:{fontSize:20,fontFamily:'Helvetica Neue'},
-	textAlign:'center',
-	width:'auto'
-});
-
-win2.add(label2);
+var table = Ti.UI.createTableView();
+table.setData(tbl_data);
 
 
 
-//
-//  add tabs
-//
-tabGroup.addTab(tab1);  
-tabGroup.addTab(tab2);  
+win1.add(table);
 
-
-// open tab group
-tabGroup.open();
+win1.open();
